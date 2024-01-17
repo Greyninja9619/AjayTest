a7705b652987c96809e0b96084129dcd3429364d
UzairKhan2598 <143694036+UzairKhan2598@users.noreply.github.com>
2024-01-17 22:56:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,4 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    if request.method == 'POST':
-        print(request.json)
-        return 'success', 200
-    else:
-        abort(400)
+    return("Hi Uzair")
